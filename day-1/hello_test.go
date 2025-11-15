044ef0aacc9176a853e56918a0408a8284a37396
zkan <zkan.cs@gmail.com>
2025-11-15 18:48:11 +0700

    Take name into func

--- day-1/hello_test.go
+++ day-1/hello_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world"
+	got := Hello("Kan")
+	want := "Hello, Kan"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
